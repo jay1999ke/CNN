Change accuracy/cost function, Fix backprop in conv layer

CNN.py
@@ -83,16 +83,24 @@ class CNN(object):
         self.activations = input_block
         return self.activations
 
-    def cost(self):
-        cross_entropy = Cost("cross_entropy")
+    def forward_test(self):
+        input_block = self.data.X_test
 
-        return cross_entropy.calculate(self.activations,self.data.y)
+        for i,layer in enumerate(self.model):       
+
+            input_block = layer.forward(input_block)
+
+        self.activations_test = input_block
+        return self.activations_test
+
+    def cost(self):
+
+        return self.get_cost_value(self.activations,self.data.y_one_hot)
 
     def backward(self):
         A = self.activations
         y = self.data.y_one_hot
-        epsilon = 0.0000001
-        error = -(y/(A+epsilon) - (1-y)/(1-A+epsilon))
+        error = y - A
 
         list_len = len(self.model)-1
         for i,layer in enumerate(self.model[::-1]):
@@ -110,10 +118,50 @@ class CNN(object):
     def fit(self,learning_rate,epochs):
         self.alpha = learning_rate
         self.epochs = epochs
-
+        print("iter","cost","train_acc","train_pred","test_acc", "test_pred"  )
         for i in range(epochs):
             self.forward()
             self.backward()
             self.update()
 
-            print(i,self.cost())
+
+            if(i%10==0):
+                self.forward_test()
+                print(i,self.cost(),
+                    self.get_accuracy_value(self.activations,self.data.y_one_hot),
+                    self.get_accuracy_value(self.activations_test,self.data.y_test_one_hot),           
+                    self.accuracy_quick(self.activations,self.data.y),
+                    self.accuracy_quick(self.activations_test,self.data.y_test)
+                
+                )
+
+    def get_accuracy_value(self,Y_hat, Y):
+        Y_hat_ = self.convert_prob_into_class(Y_hat)
+        return (Y_hat_ == Y).all(dim=1).type(torch.FloatTensor).mean()
+
+    def convert_prob_into_class(self,probs):
+        probs_ = probs.clone()
+        probs_[probs_ > 0.5] = 1
+        probs_[probs_ <= 0.5] = 0
+        return probs_
+
+    def get_cost_value(self,Y_hat, Y):
+        # number of examples
+        m = Y_hat.shape[1]
+        # calculation of the cost according to the formula
+        cost = -1 / m * (Y* torch.log(Y_hat) + (1 - Y)* torch.log(1 - Y_hat))
+        #cost = np.sum((1 / 2) * (Y - Y_hat) * (Y - Y_hat))
+        return torch.sum(torch.squeeze(cost))
+
+    def accuracy_quick(self, activation, y):
+        # calling code must set mode = 'train' or 'eval'
+        (max_vals, arg_maxs) = torch.max(activation, dim=1) 
+        # arg_maxs is tensor of indices [0, 1, 0, 2, 1, 1 . . ]
+        arg_maxs = arg_maxs 
+
+        num_correct = torch.sum(y==(arg_maxs+1)).type(torch.FloatTensor)
+        acc = num_correct /y.size()[0]
+        return acc  # percentage based
+
+
+        

layers.py
@@ -53,56 +53,27 @@ class Convolutional(object):
         delta = error * self.activation.derivative(self.activations)	        
         self.bias_grad = delta.mean(0).mean(1).mean(1).view(self.no_channels)
         
-        if isinstance(self.prev,Pooling):
-
-            try:
-                self.filter_grad = F.conv2d(input = self.prev.activations.permute(1,0,2,3),
-                    weight = delta.permute(1,0,2,3),
-                    stride = (self.stride,self.stride),
-                    padding = (self.padding,self.padding)
-                ).permute(1,0,2,3)
-
-            except:
-                self.filter_grad = F.conv2d(input = self.prev.X.permute(1,0,2,3),
-                    weight = delta.permute(1,0,2,3),
-                    stride = (self.stride,self.stride),
-                    padding = (self.padding,self.padding)
-                ).permute(1,0,2,3)
-
-            self.filter_grad = F.conv2d(input = self.prev.activations.permute(1,0,2,3),
+        try:
+            self.filter_grad = -1*F.conv2d(input = self.prev.activations.permute(1,0,2,3),
                 weight = delta.permute(1,0,2,3),
                 stride = (self.stride,self.stride),
                 padding = (self.padding,self.padding)
             ).permute(1,0,2,3)
 
-            error = F.conv_transpose2d(input = delta,
-                weight = self.filter,
-                stride=(self.stride,self.stride),
+        except:
+            self.filter_grad = -1*F.conv2d(input = self.prev.X.permute(1,0,2,3),
+                weight = delta.permute(1,0,2,3),
+                stride = (self.stride,self.stride),
                 padding = (self.padding,self.padding)
-            )
+            ).permute(1,0,2,3)
 
-        else:
 
-            try:
-                self.filter_grad = F.conv2d(input = self.prev.activations.permute(1,0,2,3),
-                    weight = delta.permute(1,0,2,3),
-                    stride = (self.stride,self.stride),
-                    padding = (self.padding,self.padding)
-                ).permute(1,0,2,3)
-
-            except:
-                self.filter_grad = F.conv2d(input = self.prev.X.permute(1,0,2,3),
-                    weight = delta.permute(1,0,2,3),
-                    stride = (self.stride,self.stride),
-                    padding = (self.padding,self.padding)
-                ).permute(1,0,2,3)
-
-            #check for derivation of prev layer error when prev="CONV"
-            error = F.conv_transpose2d(input = delta,
-                weight = self.filter,
-                stride=(self.stride,self.stride),
-                padding = (self.padding,self.padding)
-            )            
+        #check for derivation of prev layer error when prev="CONV"
+        error = F.conv_transpose2d(input = delta,
+            weight = self.filter,
+            stride=(self.stride,self.stride),
+            padding = (self.padding,self.padding)
+        )            
 
         return error
 
@@ -203,9 +174,11 @@ class Dense(object):
         if isinstance(pre, Dense):
             in_n = pre.out_dimentions[1]
         else:
-            (n_h, n_w, n_c) = pre.out_dimentions
+            try:
+                (n_h, n_w, n_c) = pre.out_dimentions
+            except:
+                (n_h, n_w, n_c) = pre.img_dim
             in_n = n_c*n_h*n_w
-
         #parameters
         self.theta = 0.09 * torch.randn(int(in_n),int(no_logits)).cuda()
         self.bias = 0.09 * torch.randn(1,no_logits).cuda()
@@ -221,32 +194,41 @@ class Dense(object):
         if not isinstance(self.prev, Dense):
             input_block = input_block.view(int(input_block.size()[0]),-1)
         self.Z = torch.mm(input_block,self.theta) + self.bias
-        self.activations = self.activation.activate(self.Z)
+        if self.activation.type == "softmax":
+            self.activations = self.activation.activate(self.Z,1)
+        else:
+            self.activations = self.activation.activate(self.Z)
         return self.activations
         
 
     def backward(self,error):
 
-        delta = error * self.activation.derivative(self.activations)
+        if(self.next != None):
+            delta = error * self.activation.derivative(self.activations)
+        else:
+            delta=error
+
         self.bias_grad = delta.mean(0).view(1,self.no_logits)
 
-        if isinstance(self.prev,Dense):
-            self.theta_grad = torch.mm(self.prev.activations.transpose(1,0),delta)
+        try:
+            a = self.prev.in_dimentions
+            test = False
+        except:
+            test = True
 
-            return torch.mm(delta,self.theta.transpose(1,0))
-        elif isinstance(self.prev,Pooling):
-            prev_activations = self.prev.activations.view(self.prev.activations.size()[0],-1).transpose(1,0)
-            self.theta_grad = torch.mm(prev_activations,delta)
+        if test:
+            prev_activations = self.prev.X.view(self.prev.X.size()[0],-1).transpose(1,0)
+            self.theta_grad = -1*torch.mm(prev_activations,delta)
 
-            return torch.mm(delta,self.theta.transpose(1,0)).view(delta.size()[0],
-                int(self.in_dimentions[2]),
-                int(self.in_dimentions[0]),
-                int(self.in_dimentions[1])
-            )
+        elif isinstance(self.prev,Dense):
+            self.theta_grad = -1*torch.mm(self.prev.activations.transpose(1,0),delta)
+
+            return torch.mm(delta,self.theta.transpose(0,1))
+
         else:
             """ grad calculation"""	
             prev_activations = self.prev.activations.view(self.prev.activations.size()[0],-1).transpose(1,0)	
-            self.theta_grad = torch.mm(prev_activations,delta)	
+            self.theta_grad = -1*torch.mm(prev_activations,delta)	
 
             """prev layer error calculation"""	
             return torch.mm(delta,self.theta.transpose(1,0)).view(delta.size()[0],	

test.py
@@ -6,6 +6,7 @@ import torch.nn.functional as op
 from layers import Convolutional, Pooling, Dense
 from CNN import CNN
 from functional import dataSet, Cost
+from sklearn.utils import shuffle
 
 if __name__ == "__main__":
 
@@ -13,11 +14,25 @@ if __name__ == "__main__":
     raw_data = mat.loadmat("mnist_reduced.mat")
 
     X = raw_data['X']
+    y = raw_data['y'].ravel()
+    X, y = shuffle(X, y, random_state=0)
+
+    X_train = X[:4000]
+    X_test = X[4000:]
+    X = X_train
+
     X_shape = X.shape
+    X_test_shape = X_test.shape
+    print(X_test_shape)
+
     X = torch.tensor(X).type(torch.FloatTensor)
+    X_test = torch.tensor(X_test).type(torch.FloatTensor)
+    
     X = X.view(X_shape[0],1,20,20)
+    X_test = X_test.view(X_test_shape[0],1,20,20)
 
     X = X.permute(0,1,3,2)
+    X_test = X_test.permute(0,1,3,2)
     """
     fig, ax = plt.subplots(nrows=2, ncols=5)
     c=0
@@ -29,18 +44,32 @@ if __name__ == "__main__":
     plt.show()"""
 
 
-    y = raw_data['y'].ravel()
+    y_train = y[:4000]
+    y_test = y[4000:]
 
     Y = np.zeros((5000, 10), dtype='uint8')
 
     for i, row in enumerate(Y):
-        Y[i, y[i] - 1] = 1
+        Y[i, y[i] -1] = 1
+
+    y = y_train
+    Y_train = Y[:4000]
+    Y_test = Y[4000:]
+    Y = Y_train
 
     y = torch.tensor(y).type(torch.LongTensor)
     Y = torch.tensor(Y).type(torch.FloatTensor)
+    y_test = torch.tensor(y_test).type(torch.LongTensor)
+    Y_test = torch.tensor(Y_test).type(torch.FloatTensor)
+    data = dataSet(X,y,(20,20,1),Y,X_test,y_test,Y_test)
 
-    data = dataSet(X,y,(20,20,1),Y)
+    NN_ARCHITECTURE = [
+        {"type": "CONV", "activation":"relu","filter_shape": (5,5),"no_channels":6,"stride":1,"padding":0},
+        {"type": "CONV", "activation":"relu","filter_shape": (3,3),"no_channels":12,"stride":1,"padding":0},
+        {"type": "Dense","activation":"softmax", "no_logits": 10},
+    ]
 
+    """
     NN_ARCHITECTURE = [
         {"type": "CONV", "activation":"relu","filter_shape": (5,5),"no_channels":6,"stride":1,"padding":0},
         {"type": "POOL","p_type":"MAX", "filter_shape": (2,2),"stride":2,"padding":0},
@@ -48,7 +77,8 @@ if __name__ == "__main__":
         {"type": "POOL","p_type":"MAX",  "filter_shape": (2,2),"stride":1,"padding":0},
         {"type": "Dense","activation":"sigmoid", "no_logits": 10},
     ]
+    """
 
     q = CNN(NN_ARCHITECTURE,data)
-    q.fit(0.00005,20)
+    q.fit(0.00001,2000)
 